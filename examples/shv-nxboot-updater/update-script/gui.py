40c892b457977b7e429c8d629a0b4aca8bcd152b
Stepan Pressl <pressl.stepan@gmail.com>
2025-12-12 03:08:49 +0100

    Add a new version of the update script, TUI based

--- examples/shv-nxboot-updater/update-script/gui.py
+++ examples/shv-nxboot-updater/update-script/gui.py
@@ -24,21 +24,13 @@ import argparse
 import asyncio
 import logging
 import sys
+import rich
+from rich.progress import Progress
+import os
 from typing import Any
 
-from PyQt6.QtCore import Qt, QThread
-from PyQt6.QtWidgets import (
-    QApplication,
-    QComboBox,
-    QDialog,
-    QGridLayout,
-    QLabel,
-    QLineEdit,
-    QProgressBar,
-    QPushButton,
-)
 from shvconfirm import shv_confirm
-from shvflasher import shv_flasher
+from shvflasher import shv_flasher, FlashStep
 
 log_levels = (
     logging.DEBUG,
@@ -48,23 +40,10 @@ log_levels = (
     logging.CRITICAL,
 )
 
-PROGRESS_STYLE = """
-QProgressBar{
-    border: 2px solid grey;
-    border-radius: 5px;
-    text-align: center
-}
-
-QProgressBar::chunk {
-    background-color: green;
-}
-"""
-
-
-def parse_args() -> argparse.Namespace:
+def get_parser():
     """Parse passed arguments and return result."""
     parser = argparse.ArgumentParser(
-        description="GUI application for NuttX firmware flash over SHV"
+        description="SHV NXboot Update Python script"
     )
     parser.add_argument(
         "-v",
@@ -102,135 +81,77 @@ def parse_args() -> argparse.Namespace:
         default="tcp://xyz@127.0.0.1:3755?password=xyz",
         help="SHV server/broker",
     )
-    return parser.parse_args()
+    parser.add_argument(
+        "-o",
+        "--operation",
+        type=str,
+        default="flash",
+        help="Requested operation of this script, supported options: flash, confirm"
+    )
+    return parser
 
-
-async def flashThreadAsync(
-    url: str, img: str, path_to_root, progress_bar: QProgressBar
-) -> None:
+async def do_flash(url: str, path_to_img: str, path_to_root: str, method_call_timeout: float):
     queue: asyncio.Queue = asyncio.Queue()
-    task = asyncio.create_task(shv_flasher(url, img, path_to_root, queue))
-
+    task = asyncio.create_task(shv_flasher(url, path_to_img, path_to_root, queue, method_call_timeout))
+    progress = None
+    task_id = None
     while True:
-        progressVal = await queue.get()
-        progress_bar.setValue(progressVal)
+        val: [FlashStep, int | str] = await queue.get()
+        match val[0]:
+            case FlashStep.INFO:
+                rich.print(f"[cyan]INFO:[/cyan] {str(val[1])}")
+            case FlashStep.WRITE_PROGRESS:
+                if progress is None:
+                    progress = Progress()
+                    progress.start()
+                    task_id = progress.add_task(f"[cyan]Flashing {os.path.basename(path_to_img)}",
+                                                total=100,
+                                                completed=0)
+                progress.update(task_id, completed=int(val[1]))
+            case FlashStep.WRITE_COMPLETE:
+                rich.print("[cyan]INFO:[/cyan] Image sent!")
+                if progress is not None and task_id is not None:
+                    progress.stop()
+                    progress.remove_task(task_id)
+                    progress = None
+                    task_id = None
+            case FlashStep.END_OK:
+                if progress is not None and task_id is not None:
+                    progress.stop()
+                    progress.remove_task(task_id)
+                rich.print(f"[green]Flashing operation succesful![/green]")
+                break
+            case FlashStep.END_ERROR:
+                if progress is not None:
+                    progress.stop()
+                    progress.remove_task(task_id)
+                rich.print(f"[red]ERROR:[/red] {str(val[1])}")
+                break
 
-        if progressVal == 100:
-            break
+async def do_confirm(url: str, path_to_root: str, method_call_timeout: float):
+    pass
 
-    await task
-
-
-class flashThread(QThread):
-    def __init__(
-        self,
-        parent: Any,
-        url: str,
-        img: str,
-        path_to_root: str,
-        progress_bar: QProgressBar,
-    ) -> None:
-        QThread.__init__(self, parent)
-        self.url = url
-        self.img = img
-        self.path_to_root = path_to_root
-        self.progress_bar = progress_bar
-
-    def run(self) -> None:
-        asyncio.run(
-            flashThreadAsync(self.url, self.img, self.path_to_root, self.progress_bar)
-        )
-
-
-class confirmThread(QThread):
-    def __init__(self, parent: Any, url: str, path_to_root: str) -> None:
-        QThread.__init__(self, parent)
-        self.url = url
-        self.path_to_root = path_to_root
-
-    def run(self) -> None:
-        asyncio.run(shv_confirm(self.url, self.path_to_root))
-
-
-class ShvFlasherGui(QDialog):
-    def __init__(self, image=None, target_mount=None, shv_server=None) -> None:
-        super().__init__()
-        self.setWindowModality(Qt.WindowModality.ApplicationModal)
-        self.setWindowTitle("NuttX Firmware Flasher over SHV")
-
-        lbl_shv_url = QLabel("SHV RCP URL")
-        self.set_interface(shv_server)
-
-        lbl_image = QLabel("Image path")
-        self.image = QLineEdit(image if image is not None else "", self)
-
-        lbl_target_mount = QLabel("Device mount")
-        self.target_mount = QLineEdit(
-            target_mount if target_mount is not None else "", self
-        )
-
-        self.progress_bar = QProgressBar(self)
-        self.progress_bar.setValue(0)
-        self.progress_bar.setStyleSheet(PROGRESS_STYLE)
-
-        pb_flash = QPushButton("FLASH")
-        pb_confirm = QPushButton("CONFIRM")
-        pb_exit = QPushButton("EXIT")
-        grid = QGridLayout()
-
-        grid.addWidget(lbl_shv_url, 0, 0)
-        grid.addWidget(self.interface, 0, 1)
-        grid.addWidget(lbl_image, 1, 0)
-        grid.addWidget(self.image, 1, 1)
-        grid.addWidget(lbl_target_mount, 2, 0)
-        grid.addWidget(self.target_mount, 2, 1)
-        grid.addWidget(self.progress_bar, 3, 0, 1, 3)
-        grid.addWidget(pb_flash, 4, 0)
-        grid.addWidget(pb_confirm, 4, 1)
-        grid.addWidget(pb_exit, 4, 2)
-
-        pb_flash.clicked.connect(self.do_flash)
-        pb_confirm.clicked.connect(self.do_confirm)
-        pb_exit.clicked.connect(self.reject)
-
-        self.setLayout(grid)
-
-    def set_interface(self, shv_server=None) -> None:
-        self.interface = QComboBox()
-        self.interface.setEditable(True)
-        # tcp://user@localhost:3755?password=pass
-        self.interface.addItems([shv_server if shv_server is not None else ""])
-
-    def do_flash(self) -> None:
-        rpc_url = str(self.interface.currentText())
-        img_path = str(self.image.text())
-        path_to_root = str(self.target_mount.text())
-        workerFlash = flashThread(
-            self, rpc_url, img_path, path_to_root, self.progress_bar
-        )
-        workerFlash.start()
-
-    def do_confirm(self) -> None:
-        rpc_url = str(self.interface.currentText())
-        path_to_root = str(self.target_mount.text())
-        worker = confirmThread(self, rpc_url, path_to_root)
-        worker.start()
-
-
-def main() -> None:
-    args = parse_args()
+def tui_main() -> None:
+    parser = get_parser()
+    args = parser.parse_args()
     logging.basicConfig(
         level=log_levels[sorted([1 - args.v + args.q, 0, len(log_levels) - 1])[1]],
         format="[%(asctime)s] [%(levelname)s] - %(message)s",
     )
-
-    _ = QApplication(sys.argv)
-
-    dialog = ShvFlasherGui(
-        image=args.image, target_mount=args.target_mount, shv_server=args.shv_server
-    )
-    dialog.exec()
-
+    if args.operation != "flash" and args.operation != "confirm":
+        rich.print(f'[red]ERROR:[/red] unsupported "{args.operation}" operation')
+        parser.print_help()
+        return
+    if args.operation == "flash" and not os.path.isfile(args.image):
+        rich.print(f"[red]ERROR:[/red] {args.image} not found")
+        return
+    # In seconds
+    METHOD_CALL_TIMEOUT = 5
+    match args.operation:
+        case "flash":
+            asyncio.run(do_flash(args.shv_server, args.image, args.target_mount, METHOD_CALL_TIMEOUT))
+        case "confirm":
+            asyncio.run(do_confirm(args.shv_server, args.target_mount, METHOD_CALL_TIMEOUT))
 
 if __name__ == "__main__":
-    main()
+    tui_main()
