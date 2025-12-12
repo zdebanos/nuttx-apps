4e7344063f488140718ccc1c48f57586b351428b
Stepan Pressl <pressl.stepan@gmail.com>
2025-12-12 03:08:49 +0100

    Add a new version of the update script, TUI based

--- examples/shv-nxboot-updater/update-script/shvconfirm.py
+++ examples/shv-nxboot-updater/update-script/shvconfirm.py
@@ -18,14 +18,41 @@
 #
 ############################################################################
 
-from shv import RpcUrl, SHVValueClient
+import asyncio
+from enum import Enum
 
-async def shv_confirm(connection: str, path_to_root: str) -> None:
-    url = RpcUrl.parse(connection)
-    client = await SHVValueClient.connect(url)
-    assert client is not None
+from shv.rpcurl import RpcUrl
+from shv.rpcapi.valueclient import SHVValueClient
 
-    await client.call(f"{path_to_root}/fwStable", "confirm")
+class ConfirmStep(Enum):
+    END_OK = 0
+    END_ERROR = 1,
 
-    print("FW confirmed.")
+async def shv_confirm(
+    connection: str, path_to_root: str, queue: asyncio.Queue,
+    method_timeout:float
+):
+    try:
+        url = RpcUrl.parse(connection)
+    except ValueError:
+        queue.put_nowait([ConfirmStep.END_ERROR, "The supplied URL is invalid!"])
+
+    try:
+        client = await asyncio.wait_for(SHVValueClient.connect(url), timeout=method_timeout)
+        if client is None:
+            _shv_flasher_cleanup("Could not connect to the broker.", queue, client)
+            return
+    except Exception as e:
+        queue.put_nowait([ConfirmStep.END_ERROR, "Could not connect to the broker. " +
+                          "Is the broker running or is the url OK?"], queue, client)
+        return
+
+    try:
+        await asyncio.wait_for(client.call(f"{path_to_root}/fwStable", "confirm"), timeout=method_timeout)
+    except TimeoutError as e:
+        queue.put_nowait([ConfirmStep.END_ERROR, "confirm timeout"])
+    except Exception:
+        queue.put_nowait([ConfirmStep.END_ERROR, "Failed calling confirm"])
+
     await client.disconnect()
+    queue.put_nowait([ConfirmStep.END_OK, 0])
--- examples/shv-nxboot-updater/update-script/shvflasher.py
+++ examples/shv-nxboot-updater/update-script/shvflasher.py
@@ -21,27 +21,61 @@
 import asyncio
 import io
 import zlib
+from enum import Enum
 
-from shv import RpcUrl, SHVBytes, SHVValueClient
+from shv import SHVBytes
+from shv.rpcapi.valueclient import SHVValueClient
+from shv.rpcurl import RpcUrl
 
+class FlashStep(Enum):
+    INFO = 0,
+    WRITE_PROGRESS = 1,
+    WRITE_COMPLETE = 2,
+    END_OK = 3,
+    END_ERROR = 4
+
+async def _shv_flasher_cleanup(msg: str, queue: asyncio.Queue, client: SHVValueClient):
+    queue.put_nowait([FlashStep.END_ERROR, str])
+    if client is not None:
+        await client.disconnect()
 
 async def shv_flasher(
-    connection: str, name: str, path_to_root: str, queue: asyncio.Queue | None
-) -> None:
-    url = RpcUrl.parse(connection)
-    client = await SHVValueClient.connect(url)
-    assert client is not None
+    connection: str, name: str, path_to_root: str, queue: asyncio.Queue | None,
+    method_timeout: float
+):
+    try:
+        url = RpcUrl.parse(connection)
+    except ValueError:
+        _shv_flasher_cleanup("The supplied URL is invalid!", queue, client)
+        return
+    queue.put_nowait([FlashStep.INFO, "Connecting to the server"])
+    try:
+        client = await asyncio.wait_for(SHVValueClient.connect(url), timeout=method_timeout)
+        if client is None:
+            _shv_flasher_cleanup("Could not connect to the broker.", queue, client)
+            return
+    except Exception as e:
+        _shv_flasher_cleanup("Could not connect to the broker. " +
+                             "Is the broker running or is the url OK?", queue, client)
+        return
+
     node_name = f"{path_to_root}/fwUpdate"
     node_name_dotdevice = f"{path_to_root}/.device"
-
-    res = await client.call(node_name, "stat")
+    try:
+        res = await client.call(node_name, "stat")
+    except TimeoutError as e:
+        _shv_flasher_cleanup("Timeout when calling the stat method.", queue, client)
+        return
+    except Exception as e:
+        _shv_flasher_cleanup("Failed calling the stat method.", queue, client)
+        return
 
     maxfilesize = res[1]
     maxwrite = res[5]
-    print(f"Received maximum enabled write size {maxwrite}.")
-    print(f"Received maximum file size is {maxfilesize}")
-    print(f"Started uploading new firmware {name}... this may take some time.")
     size = 0
+    queue.put_nowait([FlashStep.INFO, f"Received maximum enabled write size {maxwrite}."])
+    queue.put_nowait([FlashStep.INFO, f"Received maximum file size is {maxfilesize}"])
+    queue.put_nowait([FlashStep.INFO, f"Started uploading new firmware {name}."])
     with open(name, mode="rb") as f:
         # first, compute the CRC from the zlib library
         # turns out, NuttX uses the same polynomial
@@ -49,35 +83,63 @@ async def shv_flasher(
         if queue:
             f.seek(0, io.SEEK_END)
             size = f.tell()
-            print(f"File's size is {size}")
+            queue.put_nowait([FlashStep.INFO, f"File's size is {size}"])
             f.seek(0, io.SEEK_SET)
             transfers = size / maxwrite
 
         i = 0
         crc = 0
         while data := f.read(maxwrite):
-            print("Uploading chunk n.", i + 1)
             crc = zlib.crc32(data, crc)
             offset = i * maxwrite
-            res = await client.call(node_name, "write", [offset, SHVBytes(data)])
+            try:
+                res = await asyncio.wait_for(client.call(node_name, "write", [offset, SHVBytes(data)]), timeout=method_timeout)
+            except TimeoutError:
+                _shv_flasher_cleanup("Timeout when calling the write method", queue, client)
+                return
+            except Exception:
+                _shv_flasher_cleanup("Failed calling the write method.", queue, client)
+                return
             i += 1
             if queue:
                 currProgress = (int)((i * 100) / transfers)
-                queue.put_nowait(currProgress)
+                queue.put_nowait([FlashStep.WRITE_PROGRESS, currProgress])
 
-    print("Flashing completed!")
+    queue.put_nowait([FlashStep.WRITE_COMPLETE, "Flashing completed!"])
 
-    # now get the CRC from the device and reset the device, if OK
-    res = await client.call(node_name, "crc", [0, size])
+    # Now get the CRC from the device and reset the device, if OK.
+    # The crc can take a while to calculate, so set it to 20 seconds.
+    try:
+        res = await asyncio.wait_for(client.call(node_name, "crc", [0, size]), timeout=20)
+    except TimeoutError:
+        queue.put_nowait([FlashStep.END_ERROR, "Timeout when calling the crc method."])
+        await client.disconnect()
+        return
+    except Exception:
+        queue.put_nowait([FlashStep.END_ERROR, "Failed calling the crc method."])
+        await client.disconnect()
+        return
     # just to be sure, make it unsigned
     res = res & 0xFFFFFFFF
     # the result of the CRC is signed, actually, so make reinterpret it as unsigned
-    print(f"Calculated CRC: {hex(crc)} and received: {hex(res)}")
+    queue.put_nowait([FlashStep.INFO, f"Calculated CRC {hex(crc)} and received {hex(res)}"])
 
     if res == crc:
-        print("Checksum match, perform reset!")
-        res = await client.call(node_name_dotdevice, "reset")
-    else:
-        print("Checksum mismatch!")
+        queue.put_nowait([FlashStep.INFO, "Performing reset on the target device."])
+        try:
+            res = await asyncio.wait_for(client.call(node_name_dotdevice, "reset"), timeout=method_timeout)
+        except TimeoutError:
+            queue.put_nowait([FlashStep.END_ERROR, "Timeout when calling the reset method."])
+            await client.disconnect()
+            return
+        except Exception:
+            queue.put_nowait([FlashStep.END_ERROR, "Failed calling the reset method."])
+            await client.disconnect()
+            return
 
-    await client.disconnect()
+        await client.disconnect()
+        queue.put_nowait([FlashStep.END_OK, 0])
+
+    else:
+        await client.disconnect()
+        queue.put_nowait([FlashStep.END_ERROR, "CRC Mismatch!"])
